6b269c0b937ae2ae7acd104599152a1bd316b122
raphaellins <raphalinns@gmail.com>
2022-07-18 20:25:40 -0300

    Configuring Cors

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,8 @@ import session from "express-session";
 import connectRedis from "connect-redis";
 import { MyContext } from "./types";
 
+import cors from "cors";
+
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
@@ -21,6 +23,13 @@ const main = async () => {
 
   const RedisStore = connectRedis(session);
   const redisClient = redis.createClient();
+
+  app.use(
+    cors({
+      origin: "http://localhost: 3000",
+      credentials: true,
+    })
+  );
 
   app.use(
     session({
@@ -35,7 +44,7 @@ const main = async () => {
         sameSite: "lax",
         secure: __prod__, //cookie only works in https
       },
-      saveUninitialized: false, 
+      saveUninitialized: false,
       secret: "aeoiaheoihahahahhaha",
       resave: false,
     })
@@ -46,14 +55,17 @@ const main = async () => {
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: ({ req, res }): MyContext => ({
+    context: ({ req, res }) => ({
       em: orm.em,
       req,
       res,
     }),
   });
 
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
 
   app.listen(4000, () => {
     console.log("server started on localhost:4000");
